c445a0b4ad9e16f54d23e974b9514ea4916e1abc
shezan2621 <mohammadshezanislam@gmail.com>
2026-06-22 13:24:39 -0230

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,24 @@
 const music = document.getElementById("birthdayMusic");
 
-function openQR() {
-  const popup = document.getElementById("qrPopup");
-  popup.style.display = "flex";
+window.addEventListener("load", () => {
+  const items = document.querySelectorAll(".reveal");
 
-  music.volume = 0.25;
-  music.play().catch(() => {
-    console.log("Music will start after user interaction.");
+  items.forEach((item, index) => {
+    setTimeout(() => {
+      item.classList.add("show");
+    }, index * 1200 + 800);
   });
+});
+
+function openQR() {
+  document.getElementById("qrPopup").style.display = "flex";
+
+  if (music) {
+    music.volume = 0.25;
+    music.play().catch(() => {
+      console.log("Music will start after user interaction.");
+    });
+  }
 }
 
 function closeQR() {
