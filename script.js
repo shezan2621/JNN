28f34644e08257ac503f0ea874d70e9d7133d0a1
shezan2621 <mohammadshezanislam@gmail.com>
2026-06-22 13:36:17 -0230

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,5 @@
 const music = document.getElementById("birthdayMusic");
 
-window.addEventListener("load", () => {
-  const items = document.querySelectorAll(".reveal");
-
-  items.forEach((item, index) => {
-    setTimeout(() => {
-      item.classList.add("show");
-    }, index * 1200 + 800);
-  });
-});
-
 function openQR() {
   document.getElementById("qrPopup").style.display = "flex";
 
